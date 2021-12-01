3aeaef7ed08119e692e48f4b09515e83b6a75e22
Piyush Jha <piyush@trell.in>
2021-12-02 00:15:39 +0530

    Fixed video interaction buttons not working. Added URL Copying on Mobile.

--- src/components/mobile/MobileCommentButton.js
+++ src/components/mobile/MobileCommentButton.js
@@ -3,7 +3,7 @@ import {AnnotationIcon} from "@heroicons/react/solid";
 
 const MobileCommentButton = (props) => {
     return (
-        <div className="video__post-information__interaction-button">
+        <div onClick={(e)=>props.handleCommentClick(e)} className="video__post-information__interaction-button">
             <AnnotationIcon/>
             <p>{props.commentCount}</p>
         </div>
--- src/components/mobile/MobileUserFollowButton.js
+++ src/components/mobile/MobileUserFollowButton.js
@@ -3,7 +3,7 @@ import {PlusIcon} from "@heroicons/react/outline";
 
 const MobileUserFollowButton = (props) => {
     return (
-        <div className="video__post-information__interaction-button-follow">
+        <div onClick={(e)=>props.handleFollowClick(e)} className="video__post-information__interaction-button-follow">
             <img src={props.instagramImage}/>
             <div className="video__post-information__interaction-button-follow__plus">
                 <PlusIcon/>
