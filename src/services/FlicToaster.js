23b53e2cb9d44dfdac2f5475c2ac0e335977af5f
Piyush Jha <piyush@trell.in>
2021-12-15 22:40:21 +0530

    Replaced hyperlink with jsx

--- src/services/FlicToaster.js
+++ src/services/FlicToaster.js
@@ -2,28 +2,34 @@ import toast from "react-hot-toast";
 import ToastSound from "../audios/Pop-High-Round-Short-02.wav";
 
 class FlicToaster {
-  state = {
-    toastSound: new Audio(ToastSound),
-  };
+    state = {
+        toastSound: new Audio(ToastSound),
+    };
 
-  playFlicToastSound = () => {
-    this.state.toastSound.play();
-  };
+    playFlicToastSound = () => {
+        this.state.toastSound.play();
+    };
 
-  notify = (message) => {
-    this.playFlicToastSound();
-    toast(message);
-  };
+    notify = (message) => {
+        this.playFlicToastSound();
+        toast(message);
+    };
 
-  success = (message) => {
-    this.playFlicToastSound();
-    toast.success(message);
-  };
+    success = (message) => {
+        this.playFlicToastSound();
+        toast.success(message);
+    };
 
-  error = (message) => {
-    this.playFlicToastSound();
-    toast.error(message);
-  };
+    error = (message) => {
+        this.playFlicToastSound();
+        toast.error(message);
+    };
+
+    customJSX = (jsxContent) => {
+        this.playFlicToastSound();
+        toast.custom(jsxContent);
+    };
+
 }
 
 export default new FlicToaster();
