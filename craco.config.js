04c913f49a09f0feaa7fab669fd5ccc9a199943c
Sachin Kinha <codewithaisha@gmail.com>
2024-08-09 01:45:20 +0530

    update: add auto loader for sass & babel for charts

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,34 @@
+
+const CracoBabelLoader = require('craco-babel-loader');
 module.exports = {
-    style: {
-        postcss: {
-            plugins: [
-                require('autoprefixer'),
-            ],
-        },
+  style: {
+    postcss: {
+      plugins: [
+        require('autoprefixer'),
+      ],
     },
+    sass: {
+      loaderOptions: {
+        // Prefer 'sass' (dart-sass) over 'node-sass' if both packages are installed.
+        implementation: require('sass'),
+        // Workaround for this bug: https://github.com/webpack-contrib/sass-loader/issues/804
+        webpackImporter: false,
+      },
+    },
+
+  },
+  babel: {
+    plugins: [
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ],
+  },
+  plugins: [
+    {
+      plugin: CracoBabelLoader,
+      options: {
+        includes: [/(chart.js|other-dependency)/], // Specify dependencies as needed
+      },
+    },
+  ],
 }
