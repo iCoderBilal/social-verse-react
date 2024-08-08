d761df9b962c6a35f968895f9e505859bf8aa3de
Sachin Kinha <codewithaisha@gmail.com>
2024-08-09 01:56:36 +0530

    update: add admin data to store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,9 +2,12 @@ import { combineReducers } from "redux";
 import authReducer from "./auth";
 import uiReducer from "./ui";
 import catReducer from "./cat";
+import adminReducer from "./admin";
+
 
 export default combineReducers({
   auth: authReducer,
   ui: uiReducer,
   cat: catReducer,
+  admin : adminReducer
 });
