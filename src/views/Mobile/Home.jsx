ce43cc85f9b84b162135432024302f3cb1bf1624
Sachin Kinha <codewithaisha@gmail.com>
2024-08-09 02:02:11 +0530

    update: add side navigation to feed page

--- src/views/Mobile/Home.jsx
+++ src/views/Mobile/Home.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
-import Feed from "../../components/Mobile/Feed"
+import React, { useEffect, useState } from "react";
+import Feed from "../../components/Mobile/Feed";
+import MobileTopNavigation from "../../components/Mobile/TopNavigation";
+import { useDispatch } from "react-redux";
+import { setUserLoggedOut } from "../../store/auth";
+import { useNavigate } from "react-router";
+import MobileSideNavigation from "../../components/Mobile/SideNavigation";
 
 function Home(props) {
-    return (
-        <Feed/>
-    );
+  let dispatch = useDispatch();
+  let navigate = useNavigate();
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 768);
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+
+  const handleNavigationClick = () => {
+    setIsSideNavOpen(false);
+  };
+
+  return (
+    <>
+      <MobileTopNavigation
+        isSideNavOpen={isSideNavOpen}
+        setIsSideNavOpen={setIsSideNavOpen}
+      />
+
+      <div className="container">
+      <div style={{display : `${isSideNavOpen ? 'block' : 'none'} `}} onClick={()=> setIsSideNavOpen(false)} className="overlay"></div>
+        <aside className="side-bar">
+          {isMobileView ? null : (
+            <MobileSideNavigation
+              isOpen={isSideNavOpen}
+              onClose={handleNavigationClick}
+            />
+          )}
+          {isSideNavOpen && (
+            <MobileSideNavigation
+              isOpen={isSideNavOpen}
+              onClose={handleNavigationClick}
+            />
+          )}
+        </aside>
+        <main className="main-container">
+          <Feed />
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default Home;
