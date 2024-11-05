1084e2506b1951ef1ae00db5ccc2c2fc8db9aead
Jatin Dua <jdua004@gmail.com>
2024-11-05 13:52:01 +0530

    update: add doughnut chart

--- src/components/Common/Chart.jsx
+++ src/components/Common/Chart.jsx
@@ -1,32 +1,11 @@
 // AdminDashboard.js
 import React from 'react';
-import { Bar, Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, Title, Tooltip, Legend, ArcElement} from 'chart.js';
+ChartJS.register(CategoryScale, LinearScale, Title, Tooltip, Legend, ArcElement);
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement);
-
-
-export const BarChart = ({ labels = [], label = [], data = [], color = '#6b0092' }) => {
-  // Bar chart data
-  const barData = {
-    labels,
-    datasets: [
-      {
-        label,
-        data,
-        backgroundColor: color,
-        barPercentage: 0.5,
-      },
-    ],
-  };
-  return (
-    <Bar data={barData} options={{ responsive: true, plugins: { legend: { position: 'top' } } }} />
-  )
-}
-export const PieChart = ({ labels = [], data = [], color = [] }) => {
-
-  // Pie chart data
-  const pieData = {
+export const DoughnutChart = ({ labels = [], data = [], color = [] }) => {
+  const doughnutData = {
     labels,
     datasets: [
       {
@@ -41,7 +20,6 @@ export const PieChart = ({ labels = [], data = [], color = [] }) => {
     plugins: { legend: { position: 'top' } }
   }
   return (
-    <Pie data={pieData} options={options} />
+    <Doughnut data={doughnutData} options={options} />
   )
 }
-
