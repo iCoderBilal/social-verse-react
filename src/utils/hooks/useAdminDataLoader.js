670ac70a3bdeff164b330c85211d1877c7f625b5
Jatin Dua <jdua004@gmail.com>
2024-12-13 14:10:00 +0530

    update: fix react crash on loading admin dashboard

--- src/utils/hooks/useAdminDataLoader.js
+++ src/utils/hooks/useAdminDataLoader.js
@@ -1,34 +1,36 @@
 import { useEffect } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { addData ,setLoading } from "../../store/admin";
+import { addData, setLoading } from "../../store/admin";
 import FlicToaster from "../FlicToaster";
 
-
 export default function useAdminDataLoader() {
-  const {data , isLoading } = useSelector((state)=> state.admin)
+  const { data, isLoading } = useSelector((state) => state.admin);
   const dispatch = useDispatch();
-  useEffect(() => {
-    if(!isLoading && data){
-        return { data , isLoading}
-    }
-    dispatch(setLoading(true));
 
-    axios
-      .get('/admin/dashboard/data')
-      .then((response) => {
-        if(response.data.status === 'success'){
-            dispatch(addData(response.data));
-        }else{
-            FlicToaster.error("data Not fectch")
-        }
-      })
-      .catch((error) => {
-        FlicToaster.error("Error fetching Admin");
-      })
-      .finally(() => {
-        dispatch(setLoading(false));
-      });
-  }, [dispatch]);
-  return {data ,isLoading}
+  // Ensure the API is only called if data is missing and not currently loading
+  useEffect(() => {
+    if ((!data || data.length === 0) && isLoading) {
+      dispatch(setLoading(true));
+      console.log(data, isLoading);
+
+      axios
+        .get("/admin/dashboard/data")
+        .then((response) => {
+          if (response.data.status === "success") {
+            dispatch(addData(response.data)); // Store the fetched data in Redux state
+          } else {
+            FlicToaster.error("Data not fetched");
+          }
+        })
+        .catch((error) => {
+          FlicToaster.error("Error fetching Admin data");
+        })
+        .finally(() => {
+          dispatch(setLoading(false)); // Reset the loading state once API is complete
+        });
+    }
+  }, [dispatch, isLoading, data]); // Runs whenever isLoading or data changes
+
+  return { data, isLoading };
 }
