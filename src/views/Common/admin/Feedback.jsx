c2d26e58bf619bb1c2ae698e2342f774272b47e0
nidhigupta58 <gnidhi58@gmail.com>
2025-01-15 00:31:26 +0530

    update: feedback component to improve app selection and responsive design

--- src/views/Common/admin/Feedback.jsx
+++ src/views/Common/admin/Feedback.jsx
@@ -4,19 +4,40 @@ import MobileSideNavigation from "../../../components/Mobile/SideNavigation";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
+const EMPOWERVERSE = 'empowerverse';
+const VIBLE = 'vible';
+const SOLTOK = 'soltok';
+const FLIC = 'flic';
+const ETHTOK = 'ethtok';
+const FUSETRENDZ = 'fusetrendz';
+const BLOOM = 'bloom';
+const BLOOMSCROLL = 'bloomscroll';
+
 function Feedback() {
     const [isSideNavOpen, setIsSideNavOpen] = useState(false);
-    const [selectedAppName, setSelectedAppName] = useState('empowerverse');
+    const [selectedAppName, setSelectedAppName] = useState('');
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [hasMoreData, setHasMoreData] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
-    const [categories, setCategories] = useState([]);
-    const [expandedFeedbackIndex, setExpandedFeedbackIndex] = useState(null);
+    const [expandedRowIndex, setExpandedRowIndex] = useState(null);
+    const [isMobileView, setIsMobileView] = useState(window.innerWidth < 768);
 
     const navigate = useNavigate();
 
     const pageSize = 30;
+
+    const appOptions = [
+        { value: 'all', label: 'All' },
+        { value: EMPOWERVERSE, label: EMPOWERVERSE },
+        { value: VIBLE, label: VIBLE },
+        { value: SOLTOK, label: SOLTOK },
+        { value: FLIC, label: FLIC },
+        { value: ETHTOK, label: ETHTOK },
+        { value: FUSETRENDZ, label: FUSETRENDZ },
+        { value: BLOOM, label: BLOOM },
+        { value: BLOOMSCROLL, label: BLOOMSCROLL },
+    ];
 
     const fetchData = useCallback(async (page, appName) => {
         if (currentPage === page && appName === selectedAppName && data.length > 0) return;
@@ -30,7 +51,7 @@ function Feedback() {
                 params: {
                     page: currentPage,
                     page_size: pageSize,
-                    app_name: appName
+                    app_name: appName || undefined
                 },
             });
             const fetchedData = response.data.feedbacks.feedbacks;
@@ -48,13 +69,12 @@ function Feedback() {
     }, [currentPage, selectedAppName, data.length]);
 
     useEffect(() => {
-        getCategories();
         fetchData(1, selectedAppName);
     }, [currentPage, selectedAppName, fetchData]);
 
     const handleSelectChange = async (event) => {
         const value = event.target.value;
-        const appName = value.split(' ').join('').toLowerCase();
+        const appName = value === 'all' ? '' : value.split(' ').join('').toLowerCase();
         let newPage = 1;
 
         setSelectedAppName(appName);
@@ -64,24 +84,6 @@ function Feedback() {
 
         fetchData(newPage, appName);
     };
-
-    const getCategories = async () => {
-        const response = await axios.get('/categories?page=1&page_size=50');
-        // console.log(response.data);
-
-        // Sort alphabetically
-        const sortedCategories = response.data.categories.sort((a, b) => {
-            if (a.name < b.name) {
-              return -1;
-            }
-            if (a.name > b.name) {
-              return 1;
-            }
-            return 0;
-          });
-        
-        setCategories(sortedCategories);
-    }
 
     const handleScroll = (event) => {
         const { scrollTop, scrollHeight, clientHeight } = event.target;
@@ -94,9 +96,18 @@ function Feedback() {
         setIsSideNavOpen(false);
     };
 
-    const toggleDescription = (index) => {
-        setExpandedFeedbackIndex(expandedFeedbackIndex === index ? null : index);
+    const toggleRow = (index) => {
+        setExpandedRowIndex(expandedRowIndex === index ? null : index);
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobileView(window.innerWidth < 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
         <>
@@ -116,70 +127,86 @@ function Feedback() {
 
                         <div className="header-actions">
                             <button onClick={() => navigate(-1)} className="back-btn">Back</button>
-                            <select onChange={handleSelectChange} className="select">
-                                <option key={1} value='empowerverse'>Select App</option>
-                                {categories.map(category => (
-                                    <option key={category.id} value={category.name}>{category.name}</option>
+                            <select onChange={handleSelectChange} className="select" value={selectedAppName || 'all'}>
+                                {appOptions.map(option => (
+                                    <option key={option.value} value={option.value}>{option.label}</option>
                                 ))}
                             </select>
                         </div>
-                        <ul onScroll={handleScroll} style={{ height: "100vh", overflowY: "auto" }} className="feedback-list">
-                            {data && data.length > 0 ? (
-                                <>
-                                    <div className="feedback-header">
-                                        <p className="sender">User</p>
-                                        <p className="feedback-type">Type</p>
-                                        {selectedAppName === 'empowerverse' && <p className="feedback-app">AppName</p>                                        }
-                                        <p className="feedback-text">Description</p>
-                                    </div>
-                                    {
-                                        data.map((item, index) => {
-                                            const isFeedback = item.type === "F";
-                                            const isExpanded = expandedFeedbackIndex === index;
-
-                                            return (
-                                                <li key={index} className="feedback-item">
-                                                    <div className="sender">
-
-                                                        <img className="sender-image" src={item.sender.profile_picture_url} alt="" />
-                                                        <p className="sender-name">{item.sender.first_name + ' ' + item.sender.last_name}</p>
-                                                    </div>
-                                                    <div className="feedback-type">
-                                                        <p style={{ color: isFeedback ? 'black' : 'red' }}>
-                                                            {isFeedback ? "Feedback" : "Bug"}
-                                                        </p>
-                                                    </div>
-                                                    {selectedAppName === 'empowerverse' && <p className="feedback-app">{item.app_name}</p>}     
-                                                    <p className="feedback-text">
-                                                        {item.feedback.length > 100 ? (
-                                                            <>
-                                                                {isExpanded ? item.feedback : item.feedback.substring(0, 100) + '...'}
-                                                                <button 
-                                                                    onClick={() => toggleDescription(index)} 
-                                                                    style={{ 
-                                                                        border: 'none', 
-                                                                        background: 'none',
-                                                                        color: 'blue', 
-                                                                        textDecoration: 'none', 
-                                                                        cursor: 'pointer' 
-                                                                    }}
-                                                                >
-                                                                    {isExpanded ? "View Less" : "View More"}
-                                                                </button>
-                                                            </>
-                                                        ) : (
-                                                            item.feedback
-                                                        )}
-                                                    </p>
-                                                </li>
-                                            )
-                                        })
-                                    }
-                                </>
+                        <div className="table-container" onScroll={handleScroll}>
+                            {isMobileView ? (
+                                <div className="feedback-cards">
+                                    {data.map((item, index) => (
+                                        <div key={index} className="feedback-card">
+                                            <div className="feedback-header">
+                                                <div className="user-info" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                                                    <img className="sender-image" src={item.sender.profile_picture_url} alt="" />
+                                                    <span className="sender-name">{item.sender.first_name + ' ' + item.sender.last_name}</span>
+                                                </div>
+                                                <span className="feedback-type" style={{ color: item.type === "F" ? 'green' : 'red' }}>
+                                                    {item.type === "F" ? "Feature" : "Bug"}
+                                                </span>
+                                            </div>
+                                            <div className="feedback-description" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                                <span style={{ flex: 1, marginRight: '10px' }}>
+                                                    {item.feedback.length > 100 ? (
+                                                        <>
+                                                            {expandedRowIndex === index ? item.feedback : item.feedback.substring(0, 100) + '...'}
+                                                            {expandedRowIndex === index ? (
+                                                                <span onClick={() => toggleRow(index)} style={{ cursor: 'pointer', marginLeft: '10px' }}>↑</span>
+                                                            ) : (
+                                                                <span onClick={() => toggleRow(index)} style={{ cursor: 'pointer', marginLeft: '10px' }}>↓</span>
+                                                            )}
+                                                        </>
+                                                    ) : (
+                                                        item.feedback
+                                                    )}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
                             ) : (
-                                <li>No data available</li>
+                                <table className="feedback-table">
+                                    <thead>
+                                        <tr>
+                                            <th>User</th>
+                                            <th>Type</th>
+                                            {selectedAppName === 'empowerverse' && <th>App Name</th>}
+                                            <th>Description</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {data.map((item, index) => (
+                                            <tr key={index}>
+                                                <td className="feedback-user-info">
+                                                    <div className="user-info" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                                                        <img className="sender-image" src={item.sender.profile_picture_url} alt="" />
+                                                        <span className="sender-name">{item.sender.first_name + ' ' + item.sender.last_name}</span>
+                                                    </div>
+                                                </td>
+                                                <td style={{ color: item.type === "F" ? 'black' : 'red' }}>
+                                                    {item.type === "F" ? "Feedback" : "Bug"}
+                                                </td>
+                                                {selectedAppName === 'empowerverse' && <td>{item.app_name}</td>}
+                                                <td>
+                                                    {item.feedback.length > 100 ? (
+                                                        <>
+                                                            {expandedRowIndex === index ? item.feedback : item.feedback.substring(0, 100) + '...'}
+                                                            <span onClick={() => toggleRow(index)} style={{ cursor: 'pointer' }}>
+                                                                {expandedRowIndex === index ? '↑' : '↓'}
+                                                            </span>
+                                                        </>
+                                                    ) : (
+                                                        item.feedback
+                                                    )}
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
                             )}
-                        </ul>
+                        </div>
                     </div>
                 </main>
             </div>
